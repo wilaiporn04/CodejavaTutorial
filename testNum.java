5c10efc038d07ac06feabcff0224d0ec98579ab9
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-07-25 14:37:27 +0700

    Create testNum.java

---
+++ testNum.java
@@ -0,0 +1,11 @@
+public class testNum {
+    static void main(String args[]){
+        int x,  y; //ประกาศตัวแปรเป็นจำนวนเต็ม
+        x = 10;
+        y = x+ 1 ;
+        System.out.println("x = " + x+"y = " +y);
+        y = y++1 ;
+        System.out.println("x = " + x+"y = " +y);
+    }
+
+}
