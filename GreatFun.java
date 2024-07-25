80835dd042269841fde880e6c5ba691b11a1b5a2
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-07-25 14:37:20 +0700

    Create GreatFun.java

---
+++ GreatFun.java
@@ -0,0 +1,8 @@
+public class GreatFun {
+    public static void main(String[] args){
+        System.out.print("Program is");
+        System.out.println("Great Fun");
+        System.out.print("These ase our sellers:");
+        
+    }
+}
