cbd79ef275eb5a9185539ba63769c0415a34c87d
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-07-25 14:37:15 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String [] args){
+        int x,y;
+        x = 62;
+        y = 99;
+        float total;
+        total = x + y;
+        System.out.println("EnterNumber 62 + 99 " +total);
+    }
+    
+}
