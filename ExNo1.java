f6b76c5b959d0b0244a15bd557e26ce9fe805ab2
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-08-01 14:22:13 +0700

    Create ExNo1.java

---
+++ ExNo1.java
@@ -0,0 +1,9 @@
+public class ExNo1 {
+    public static void main(String[] args) {
+    
+        System.out.println("เลขคู่จาก 2 ถึง 100 คือ:");
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
