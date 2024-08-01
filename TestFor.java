9a27666109e894ce2c3463c02d0563cc97412a05
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-08-01 10:51:52 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,7 @@
+public class TestFor {
+    public static void main(String[] args){
+        for (int i =1; i <= 12; i++) {
+            System.out.println("2 x " + i + " = " + (2 *i));
+        }
+    }
+}
