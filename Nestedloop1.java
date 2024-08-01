7b36ae99903781df12789199d8072459d06826d5
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-08-01 11:23:26 +0700

    Create Nestedloop1.java

---
+++ Nestedloop1.java
@@ -0,0 +1,9 @@
+public class Nestedloop1 {
+    public static void main(String[] args){
+        for(int i=1;i<=3;i++){
+            for(int j=1; i<=3; j++){
+                System.out.println(j+"");
+            }
+        }
+    }
+}
