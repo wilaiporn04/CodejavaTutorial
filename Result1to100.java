4a541eecc6f9948334f7c4581bc8ddf79b826bc3
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-08-01 10:22:56 +0700

    Create Result1to100.java

---
+++ Result1to100.java
@@ -0,0 +1,10 @@
+public class Result1to100 {
+    public static void main(String[] args) {
+        int ying = 0, ball = 1;
+        while (ball <= 100){
+           ying += ball;
+            ball++ ;
+    }
+    System.out.println("ผลลับ 1  ถึง 100 :"+ying);
+}
+}
