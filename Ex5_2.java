ff64d0b81b76f643b8e390a583e1fe3390471180
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-08-05 10:45:16 +0700

    Create Ex5_2.java

---
+++ Ex5_2.java
@@ -0,0 +1,12 @@
+
+public class Ex5_2 {
+    public static void main(String[] args ){
+        for (int x =1; x <=10; x++) {
+            int  cube = x*x*x;
+            System.out.println("The cube of " + x +" is+ cube");
+        
+      
+
+}
+}
+}
