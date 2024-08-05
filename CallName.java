7d5699a9f1db9608cc1af3bb572121f301190709
wilaiporn04 <152351833+wilaiporn04@users.noreply.github.com>
2024-08-05 10:45:09 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,11 @@
+public class CallName {
+    public static void main(String [] args){
+        for(int i =1; i <= 10; i++) {
+             name();
+
+        }
+    }
+    public static void name(){
+        System.out.println("Wilaiporn Netsuwan");
+    }
+}
